f6cc11fe78607c55759e2a5d0ff7cc45182de8d6
chrisbekk <christianbekkelund@gmail.com>
2023-05-01 14:13:32 +0200

    Started working on the featured articles card

--- public/index.js
+++ public/index.js
@@ -1,22 +1,9 @@
-import navBar from "../src/navBar.js";
+import navBar from "../components/navBar.js";
 import fetchAPI from "../utils/dataFetching.js";
-const element = document.querySelector(".circle")
 const container = document.querySelector("svg")
 const buttons = container.querySelectorAll("rect")
 
-function heroAnimation(e){
-    element.style.display = "block"
-    let x = e.clientX
-    let y = e.clientY
-    
-    element.style.setProperty("--mouse-x", `${x}px`);
-    element.style.setProperty("--mouse-y", `${y}px`);
 
-    container.addEventListener("mouseleave", e=>{
-        element.style.display = "none"
-    })
-
-}
 function animateButton(e){
     console.log(e.target.dataset.nofill)
     if(e.target.dataset.nofill == "false"){
@@ -34,7 +21,7 @@ function handleEnterButton(e){
 
 buttons.forEach(button => button.onmouseenter = e => animateButton(e))
 buttons.forEach(button => button.onclick = e => handleEnterButton(e))
-container.onmousemove = e => heroAnimation(e)
+
 
 
 async function main(){
@@ -55,48 +42,7 @@ lol.addEventListener("mouseenter", (e)=>{
     })
 })
 
-// const leftBtn = document.querySelector("#left-arrow")
-// const rightBtn = document.querySelector("#right-arrow")
-// let currentArticle = 0
-// leftBtn.addEventListener("click", (e)=>{
-//     if(currentArticle === 0){
-//         return
-//     }
-//     currentArticle -= 1
-//     console.log(currentArticle)
-// })
 
-// rightBtn.addEventListener("click", (e)=>{
-//     if(currentArticle === 3){
-//         return
-//     }
-//     currentArticle += 1
-//     console.log(currentArticle)
-// })
-
-// const arrays = ["omg", "te", "212"]
-
-// function tass(){
-//     let currentArticle = 0
-//     console.log(arrays[currentArticle])
-//     leftBtn.addEventListener("click", (e)=>{
-//         if(currentArticle === 0){
-//             return
-//         }
-//         currentArticle -= 1
-//         console.log(arrays[currentArticle])
-//     })
-    
-//     rightBtn.addEventListener("click", (e)=>{
-//         if(currentArticle === arrays.length-1){
-//             return
-//         }
-//         currentArticle += 1
-//         console.log(arrays[currentArticle])
-//     })
-// }
-
-// tass()
 
 /* GET GRID HTML ELEMENTS */ 
 const featuredImageContainer = document.querySelector(".grid-featured-image")
@@ -155,8 +101,10 @@ function renderFeaturedArticles(posts){
     })
     leftBtn.addEventListener("click", (e)=>{
         if(currentArticle === 0){
+            
             return
         }
+        
         currentArticle -= 1
         featuredImageContainer.innerHTML = ""
         featuredDetailsContainer.innerHTML = ""
@@ -167,8 +115,10 @@ function renderFeaturedArticles(posts){
     })
     rightBtn.addEventListener("click", (e)=>{
         if(currentArticle === posts.length-1){
+            
             return
         }
+        
         currentArticle += 1
         featuredImageContainer.innerHTML = ""
         featuredDetailsContainer.innerHTML = ""
